f4eb3eb704775a45b7d4d284bbc33738082e8602
Satyam Banwale <satyambanwale@prodios.com>
2024-09-26 23:28:19 +0530

    added orders route, service, schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { SaloonForWomenModule } from "./saloon/saloon-for-women/salon-for-women.module";
 import { TypegooseModule } from "@m8a/nestjs-typegoose";
+import { OrdersModule } from "./orders/orders.module";
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TypegooseModule } from "@m8a/nestjs-typegoose";
       "mongodb+srv://satyambanwale786:satyam786@villa.njlqb.mongodb.net/villa"
     ),
     SaloonForWomenModule,
+    OrdersModule
   ],
   controllers: [AppController],
   providers: [AppService],
